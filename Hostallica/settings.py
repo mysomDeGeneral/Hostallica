d93cac2b77632c47ec46c54025bb8e94c39f86ca
Fianyeku-Kokonu Michael <fianyekukokonumichael@gmail.com>
2023-07-23 21:13:25 +0000

    update settings.py

--- Hostallica/settings.py
+++ Hostallica/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATTIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
