e85e6efb03e088bd2af0ba44f7a480004ca44bd4
Fianyeku-Kokonu Michael <fianyekukokonumichael@gmail.com>
2023-07-24 02:30:18 +0000

    update settings.py

--- Hostallica/settings.py
+++ Hostallica/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn", "static_root")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_files", "static_root")
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_files", "media_root")
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
