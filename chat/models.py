f27362c676f56d55d30b3eaec90b5dcc6bf58a0c
Fianyeku-Kokonu Michael <fianyekukokonumichael@gmail.com>
2023-07-24 19:37:14 +0000

    chat app

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from backend.models import Student,Room
 
 # Create your models here.
+class Message(models.Model):
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from backend.models import Student, Hall, Room
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'chathome.html')
+
+
+def room(request, room):
+    return render(request, 'chatroom.html')
