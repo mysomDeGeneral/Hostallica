c8251549b08e99013d71cda81564b7aac8d402b5
Fianyeku-Kokonu Michael <fianyekukokonumichael@gmail.com>
2023-07-24 09:10:00 +0000

    whitenoise configuration

--- Hostallica/settings.py
+++ Hostallica/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn", "static_root")
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
