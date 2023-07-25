2ab474fcf641c550c168861d6b8f4dbd6a039a26
Fianyeku-Kokonu Michael <fianyekukokonumichael@gmail.com>
2023-07-25 18:47:55 +0000

    change deafult file storage

--- Hostallica/settings.py
+++ Hostallica/settings.py
@@ -168,7 +168,7 @@ WHITENOISE_USE_FINDERS = False
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles", "media_root")
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
